59c3667747956317636d9d9f09ccad4dd56f2c97
Harshdeep Sokhey <hsokhey@buffalo.edu>
2018-07-23 21:58:50 -0400

    Added code from pyimagesearch ref

---
+++ PA3/rgbhistogram.py
@@ -0,0 +1,28 @@
+# Image based search engine
+# @Ref: https://www.pyimagesearch.com/2014/01/27/hobbits-and-histograms-a-how-to-guide-to-building-your-first-image-search-engine-in-python/
+import cv2
+
+class RGBHistogram:
+    def __init__(self, bins):
+        # store the number of bins the histogram will use
+        self.bins = bins
+ 
+    def describe(self, image):
+        # compute a 3D histogram in the RGB colorspace,
+        # then normalize the histogram so that images
+        # with the same content, but either scaled larger
+        # or smaller will have (roughly) the same histogram
+        hist = cv2.calcHist([image], [0, 1, 2],
+            None, self.bins, [0, 256, 0, 256, 0, 256])
+ 
+        # normalize with OpenCV 2.4
+        if imutils.is_cv2():
+            hist = cv2.normalize(hist)
+ 
+        # otherwise normalize with OpenCV 3+
+        else:
+            hist = cv2.normalize(hist,hist)
+ 
+        # return out 3D histogram as a flattened array
+        return hist.flatten()
+
